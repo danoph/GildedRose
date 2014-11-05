ac412ff64d66134573f591b5ca248dd46c1a7499
Daniel Errante <daniel.errante@asynchrony.com>
2014-11-05 14:57:37 -0600

    making new classes

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -7,60 +7,24 @@ class GildedRose
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
-    @items << Item.new("Conjured Item", 4, 8)
+    @items << NormalItem.new("+5 Dexterity Vest", 10, 20)
+    @items << AgedBrie.new("Aged Brie", 2, 0)
+    @items << NormalItem.new("Elixir of the Mongoose", 5, 7)
+    @items << SulfurasItem.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << ConjuredItem.new("Conjured Mana Cake", 3, 6)
+    @items << ConjuredItem.new("Conjured Item", 4, 8)
   end
 
   def update_quality
     @items.each do |item|
-      update_item_quality_and_sell_in(item)
+      update_item_quality(item)
+      update_item_sell_in(item)
     end
-  end
-
-  def update_item_quality_and_sell_in(item)
-    update_item_quality(item)
-    update_item_sell_in(item)
   end
 
   def update_item_quality(item)
-    if item.name =~ /conjured/i
-      subtract_quality(item, 2)
-    elsif item.name == "Sulfuras, Hand of Ragnaros"
-      # dont update
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-      add_quality(item)
-
-      if (item.sell_in < 11)
-        add_quality(item)
-      end
-      if (item.sell_in < 6)
-        add_quality(item)
-      end
-      if item.sell_in <= 0
-        item.quality = 0
-      end
-    elsif item.name == "Aged Brie"
-      add_quality(item)
-    else
-      subtract_quality(item)
-
-      if (item.sell_in <= 0)
-        subtract_quality(item)
-      end
-    end
-  end
-
-  def add_quality(item, amount=1)
-    item.quality += amount if item.quality < 50
-  end
-
-  def subtract_quality(item, amount=1)
-    item.quality = item.quality - amount if item.quality > 0
+    item.quality += item.quality_adjustment if item.quality + item.quality_adjustment >= 0 && item.quality + item.quality_adjustment <= 50
   end
 
   def update_item_sell_in(item)
--- item.rb
+++ item.rb
@@ -1,12 +1,57 @@
 class Item
-
   attr_accessor :name, :sell_in, :quality
-        
+
   def initialize (name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
+end
 
+class NormalItem < Item
+  def quality_adjustment
+    adjust = -1
+    adjust -= 1 if sell_in <= 0
+    adjust
+  end
+end
 
+class AgedBrie < Item
+  def quality_adjustment
+    1
+  end
+end
+
+class ConjuredItem < NormalItem
+  def quality_adjustment
+    super * 2
+  end
+end
+
+class SulfurasItem < Item
+  def quality_adjustment
+    0
+  end
+
+  def quality
+    80
+  end
+end
+
+class BackstagePass < Item
+  def quality_adjustment
+    adjust = 1
+
+    if (sell_in < 11)
+      adjust += 1
+    end
+    if (sell_in < 6)
+      adjust += 1
+    end
+    if sell_in <= 0
+      adjust = -quality
+    end
+
+    adjust
+  end
 end
