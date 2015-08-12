8564ae33950f5298b69bde499253156d85f306ad
Joe Hainline <joe.hainline@asynchrony.com>
2015-08-12 12:06:51 -0500

    gene doyel

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,5 +1,86 @@
 require './item.rb'
 
+class NormalItem
+  def tick
+    subtract_quality
+    self.sell_in -= 1
+
+    subtract_quality if sell_in < 0
+  end
+
+  def add_quality
+    self.quality += 1 if quality < 50
+  end
+
+  def subtract_quality
+    self.quality -= 1 if quality > 0
+  end
+end
+
+class Sulfuras < NormalItem
+
+  def tick
+  end
+
+end
+
+class AgedBrie < NormalItem
+
+  def tick
+    add_quality
+    add_quality if sell_in < 0
+    self.sell_in -= 1
+  end
+
+
+end
+
+class BackstagePasses < NormalItem
+
+  def tick
+    add_quality
+    add_quality if sell_in < 11
+    add_quality if sell_in < 6
+
+    self.sell_in -= 1
+
+    self.quality = 0 if sell_in < 0
+  end
+
+end
+
+class ConjuredItem < NormalItem
+
+  def tick
+    subtract_quality
+    subtract_quality
+    self.sell_in -= 1
+    subtract_quality if sell_in < 0
+  end
+
+end
+
+class ItemFactory
+
+  def build(item)
+    klass = case item.name
+    when "Aged Brie"
+      AgedBrie
+    when "Sulfuras, Hand of Ragnaros"
+      Sulfuras
+    when "Backstage passes to a TAFKAL80ETC concert"
+      BackstagePasses
+    when /conjured/i
+      ConjuredItem
+    else
+      NormalItem
+    end
+
+    klass.new(item.name, item.sell_in, item.quality)
+  end
+
+end
+
 class GildedRose
 
   @items = []
@@ -14,54 +95,18 @@ class GildedRose
     @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
     @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
     @items << Item.new("Conjured Mana Cake", 3, 6)
+    @items << Item.new("Another Conjured Item #2", 3, 6)
   end
 
   def update_quality
+    factory = ItemFactory.new
 
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
-          end
-        end
-      else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-          end
-        end
-      end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
-      end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
-              end
-            end
-          else
-            @items[i].quality = @items[i].quality - @items[i].quality
-          end
-        else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
-          end
-        end
-      end
+    @items = items.map do |item|
+      factory.build(item)
+    end
+
+    items.each do |item|
+      item.tick
     end
   end
 
