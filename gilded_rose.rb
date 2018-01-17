c7b2dd95d7c31ae0dd7ec230cdcaa3f38e90aaa2
Daniel Errante <danoph@gmail.com>
2018-01-17 16:41:56 -0600

    used polymorphism to refactor complicated conditional

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -8,62 +8,17 @@ class GildedRose
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
-  end
-
-  def increase_quality(item)
-    if (item.quality < 50)
-      item.quality = item.quality + 1
-    end
-  end
-
-  def decrease_quality(item)
-    if (item.quality > 0)
-      if (item.name != "Sulfuras, Hand of Ragnaros")
-        item.quality = item.quality - 1
-      end
-    end
-  end
-
-  def decrease_sell_in(item)
-    if (item.name != "Sulfuras, Hand of Ragnaros")
-      item.sell_in = item.sell_in - 1;
-    end
+    @items << DexterityVest.new("+5 Dexterity Vest", 10, 20)
+    @items << AgedBrie.new("Aged Brie", 2, 0)
+    @items << MongooseElixir.new("Elixir of the Mongoose", 5, 7)
+    @items << Sulfuras.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << ManaCake.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
-
     @items.each do |item|
-      if item.name == "Aged Brie"
-        increase_quality(item)
-        if (item.sell_in <= 0)
-          increase_quality(item)
-        end
-      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-        increase_quality(item)
-        if (item.sell_in < 11)
-          increase_quality(item)
-        end
-        if (item.sell_in < 6)
-          increase_quality(item)
-        end
-        if item.sell_in <= 0
-          item.quality = 0
-        end
-      else
-        decrease_quality(item)
-        if (item.sell_in <= 0)
-          decrease_quality(item)
-        end
-      end
-
-      decrease_sell_in(item)
-
+      item.update_quality
     end
   end
 
--- item.rb
+++ item.rb
@@ -2,11 +2,87 @@ class Item
 
   attr_accessor :name, :sell_in, :quality
 
-  def initialize (name, sell_in, quality)
+  def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
 
+  def increase_quality
+    if quality < 50
+      self.quality = quality + 1
+    end
+  end
+
+  def decrease_quality
+    if quality > 0
+      self.quality = quality - 1
+    end
+  end
+
+  def decrease_sell_in
+    self.sell_in = sell_in - 1
+  end
+
+  def update_quality
+    decrease_quality
+
+    if sell_in <= 0
+      decrease_quality
+    end
+
+    decrease_sell_in
+  end
+end
+
+class AgedBrie < Item
+  def update_quality
+    increase_quality
+
+    if sell_in <= 0
+      increase_quality
+    end
+
+    decrease_sell_in
+  end
+end
+
+class DexterityVest < Item
+
+end
+
+class MongooseElixir < Item
+
+end
+
+class Sulfuras < Item
+  def decrease_sell_in
+  end
+
+  def decrease_quality
+  end
+end
+
+class BackstagePass < Item
+  def update_quality
+    increase_quality
+
+    if sell_in < 11
+      increase_quality
+    end
+
+    if sell_in < 6
+      increase_quality
+    end
+
+    if sell_in <= 0
+      self.quality = 0
+    end
+
+    decrease_sell_in
+  end
+end
+
+class ManaCake < Item
 
 end
